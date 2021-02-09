b264953696428c6507a15b9e6c2be5d246c17679
m7mdxdgn999 <42533116+m7mdxdgn999@users.noreply.github.com>
2021-02-09 11:24:27 +0700

    validation

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -10,22 +10,33 @@
             <h1>Form Tambah mahasiswa</h1>
 
             <form action="/students" method="post">
-             @csrf
+                @csrf
                 <div class="mb-3">
                     <label for="nama" class="form-label">Nama</label>
-                    <input type="text" class="form-control" id="nama" placeholder="Masukan Nama" name="nama" >
+                    <input type="text" class="form-control @error ('nama') is-invalid @enderror" id="nama" placeholder="Masukan Nama" name="nama">
+                    @error ('nama')
+                    <div id="validationServerUsernameFeedback" class="invalid-feedback">
+                        {{ $massage ?? 'pleade fill the name' }}
+                    </div>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="nim" class="form-label">NIM</label>
-                    <input type="text" class="form-control" id="nim" placeholder="Masukan NIM" name="nim" >
+                    <input type="text" class="form-control @error ('nim') is-invalid @enderror" id="nim" placeholder="Masukan NIM" name="nim">
+                    @error ('nama')
+                    <div id="validationServerUsernameFeedback" class="invalid-feedback">
+                        {{ $massage ?? 'please fill the nim' }}
+                    </div>
+                    @enderror
+
                 </div>
                 <div class="mb-3">
-                    <label for="jurusan" class="form-label">jurusan</label> 
-                    <input type="text" class="form-control" id="jurusan" placeholder="Masukan jurusan" name="jurusan" >
+                    <label for="jurusan" class="form-label">jurusan</label>
+                    <input type="text" class="form-control" id="jurusan" placeholder="Masukan jurusan" name="jurusan">
                 </div>
                 <div class="mb-3">
                     <label for="email" class="form-label">email</label>
-                    <input type="text" class="form-control" id="email" placeholder="Masukan email" name="email" >
+                    <input type="text" class="form-control" id="email" placeholder="Masukan email" name="email">
                 </div>
                 <button type="submit" class="btn btn-primary">Tambah Data</button>
             </form>
